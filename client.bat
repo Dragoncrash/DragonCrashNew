3a56bf971f214f9d9811e537568041adb05fd495
Edmond T Dooley <etdooley@crm.ucsc.edu>
2017-06-06 15:42:39 -0700

    Further improvements to the batch files. They now store the editor path in a variable for easy changing and the client accepts the server IP as input.

--- client.bat
+++ client.bat
@@ -1,1 +1,6 @@
-"C:\Program Files (x86)\Epic Games\4.14\Engine\Binaries\Win64\UE4Editor.exe" %~dp0\DragonCrashNew.uproject 127.0.0.1 -game
+@echo off
+set editorPath="../Engine/Binaries/Win64/UE4Editor.exe"
+set serverIP=127.0.0.1
+set /p serverIP=Enter Server IP Address (Default = 127.0.0.1): 
+echo Connecting to server at %serverIP%...
+%editorPath% %~dp0\DragonCrashNew.uproject %serverIP% -game
--- dedicated_server.bat
+++ dedicated_server.bat
@@ -1,1 +1,4 @@
-"C:\Program Files (x86)\Epic Games\4.14\Engine\Binaries\Win64\UE4Editor.exe" %~dp0\DragonCrashNew.uproject -server -game -log
+@echo off
+set editorPath="../Engine/Binaries/Win64/UE4Editor.exe"
+echo Starting server...
+%editorPath% %~dp0\DragonCrashNew.uproject -server -game -log
