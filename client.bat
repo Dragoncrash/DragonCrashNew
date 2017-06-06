0daed961e153a4c36ab0e74c2fce0ad1ff9434a5
Edmond T Dooley <etdooley@crm.ucsc.edu>
2017-06-06 15:02:38 -0700

    Removed/Fixed old environment files that we are no longer using. Change game so it restarts when a player wins rather than proceeding to victory screen. Reimported and created animation blueprint for Pango. Started creating Dragon class for Pango. Added improved batch files. They now only require you to add the UE4Editor.exe path.

---
+++ client.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Epic Games\4.14\Engine\Binaries\Win64\UE4Editor.exe" %~dp0\DragonCrashNew.uproject 127.0.0.1 -game
---
+++ dedicated_server.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Epic Games\4.14\Engine\Binaries\Win64\UE4Editor.exe" %~dp0\DragonCrashNew.uproject -server -game -log
