c14636b1ac9cde269480dfae1175da45624e68c4
Edmond T Dooley <etdooley@crm.ucsc.edu>
2017-06-12 15:10:13 -0700

    Merge etdooley_branch into master.

--- Source/DragonCrashNew/DragonCrashNew.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "DragonCrashNew.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DragonCrashNew, "DragonCrashNew" );
-
-DEFINE_LOG_CATEGORY(ConfigFunctions);
